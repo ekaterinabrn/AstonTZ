66c858bac1f68d1e308ee5b122ebb8b279dce42c
Ekaterina Briuchanova <ueobrn@yandex.ru>
2024-09-14 12:16:25 +0700

    create firs tz

--- src/main/java/ATZ/TwoNumbers.java
+++ src/main/java/ATZ/TwoNumbers.java
@@ -3,15 +3,24 @@ import java.util.Scanner;
 
 public class TwoNumbers {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("ведите число");
-    int a = sc.nextInt();
+        int a = sc.nextInt();
         System.out.println("ведите число");
         int b = sc.nextInt();
         if(a>b){System.out.println(a  + ">" + b);}
-     else if (a < b) {
-        System.out.println(a + " < " + b);
-    } else {
-        System.out.println(a + " = " + b);
-    }
-}}
+        else if (a < b) {
+            System.out.println(a + " < " + b);
+        } else {
+            System.out.println(a + " = " + b);
+        }
+        int sum = a+b;
+        System.out.println("Сумма чисел " + sum);
+        int difference= a-b;
+        System.out.println("Разность чисел " + difference);
+        int composition = a*b;
+        System.out.println("Произведение чисел " + composition);
+
+        try{ int division = a/b; System.out.println("Деление чисел " + division);}
+       catch (ArithmeticException e){ System.out.println("Деление на ноль невозможно ");
+    }}}
