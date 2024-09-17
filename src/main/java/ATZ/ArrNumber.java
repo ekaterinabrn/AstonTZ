18d2b6c2ca2eb19c74a3aadd099c3a652322beb8
Ekaterina Briuchanova <ueobrn@yandex.ru>
2024-09-17 17:51:26 +0700

    create object

--- src/main/java/ATZ/ArrNumber.java
+++ src/main/java/ATZ/ArrNumber.java
@@ -1,8 +1,9 @@
 package ATZ;
 
 public class ArrNumber {
-    private static final  int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    public static  void getEvenNumbers(){
+    private static final int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    public static void getEvenNumbers() {
         for (int number : numbers) {
             if (number % 2 == 0) {
                 System.out.println(number);
@@ -10,3 +11,5 @@ public class ArrNumber {
         }
     }
 }
+
+
--- src/main/java/ATZ/Main.java
+++ src/main/java/ATZ/Main.java
@@ -2,8 +2,10 @@ package ATZ;
 
 public class Main {
     public static void main(String[] args) {
-        TwoNumbers.numberScanner();
-        TwoStrings.stringScaner();
+        TwoNumbers twoNumbers=new TwoNumbers();
+        twoNumbers.numberScanner();
+        TwoStrings twoStrings= new TwoStrings();
+        twoStrings.stringScaner();
        ArrNumber.getEvenNumbers();
     }
 }
